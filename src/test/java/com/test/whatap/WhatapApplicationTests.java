c5192eca3166fd8b055fa5a1f46b072391ef496e
rnjsehdgks01 <62535103+rnjsehdgks01@users.noreply.github.com>
2021-10-06 16:13:37 +0900

    Initial commit

---
+++ src/test/java/com/test/whatap/WhatapApplicationTests.java
@@ -0,0 +1,13 @@
+package com.test.whatap;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WhatapApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
